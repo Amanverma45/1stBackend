6cd965f41c795c00ffed1b42935fe869fd257d5e
Aman Verma <av478136@gmail.com>
2025-09-17 14:58:34 +0530

    helo90

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 require('./db/Connection')
 const app =  express()
 app.use(cors({
-    origin:['https://frontendemployee.netlify.app/'],
+    origin:['https://frontendemployee.netlify.app'],
     credentials:true
 }));
 const PORT = 5000
